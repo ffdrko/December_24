b7625d99357703d9550db2feb339d1057ddcbdaa
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2024-12-01 12:12:23 +0600

    1st commit

---
+++ Task_Management/main.py
@@ -0,0 +1,8 @@
+prompt = "Enter a todo: "
+
+user_task_1 = input(prompt)
+user_task_2 = input(prompt)
+user_task_3 = input(prompt)
+
+todo_list = [user_task_1, user_task_2, user_task_3]
+print(todo_list)
