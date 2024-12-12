df64274b884f10cb3ee450527cecc7aa6a963032
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2024-12-12 12:43:50 +0600

    8th commit

--- Task_Management/main.py
+++ Task_Management/main.py
@@ -19,13 +19,30 @@ while True:
             todo_list = file.readlines()
             file.close()
             for index, task in enumerate(todo_list):
-                print(f"{index + 1}-{task}")
-        # case 'edit':
-        #     todo_num = int(input("Enter your todo number: ")) - 1
-        #     todo_list[todo_num] = input("Enter your new todo: ")
-        # case 'complete':
-        #     todo_num = int(input("Enter your todo number: ")) - 1
-        #     todo_list.pop(todo_num)
+                print(f"{index + 1}-{task.strip('\n')}")
+        case 'edit':
+            file = open("Files/todo_list.txt", "r")
+            todo_list = file.readlines()
+            file.close()
+
+            todo_num = int(input("Enter your todo number: ")) - 1
+            todo_list[todo_num] = input("Enter your new todo: ") + '\n'
+
+            file = open("Files/todo_list.txt", "w")
+            file.writelines(todo_list)
+            file.close()
+
+        case 'complete':
+            file = open("Files/todo_list.txt", "r")
+            todo_list = file.readlines()
+            file.close()
+
+            todo_num = int(input("Enter your todo number: ")) - 1
+            todo_list.pop(todo_num)
+
+            file = open("Files/todo_list.txt", "w")
+            file.writelines(todo_list)
+            file.close()
         case "exit":
             break
 
