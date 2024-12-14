3c877daf9d2b6661f005aff9cb60a22324321662
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2024-12-14 13:20:23 +0600

    10th commit

--- Task_Management/main.py
+++ Task_Management/main.py
@@ -1,43 +1,48 @@
 while True:
     user_action = input("Type add, show, edit, complete or exit: ").strip()
 
-    match user_action:
-        case "add":
-            with open('Files/todo_list.txt') as file:
-                todo_list = file.readlines()
+    if 'add' in user_action:
+        with open('Files/todo_list.txt') as file:
+            todo_list = file.readlines()
 
-            task = input("Enter a todo: ") + "\n"
-            todo_list.append(task)
+        task = input("Enter a todo: ") + "\n"
+        todo_list.append(task)
 
-            with open('Files/todo_list.txt', 'w') as file:
-                file.writelines(todo_list)
+        with open('Files/todo_list.txt', 'w') as file:
+            file.writelines(todo_list)
 
-        case "show":
-            with open('Files/todo_list.txt') as file:
-                todo_list = file.readlines()
+    elif 'show' in user_action:
+        with open('Files/todo_list.txt') as file:
+            todo_list = file.readlines()
 
-            for index, task in enumerate(todo_list):
-                print(f"{index + 1}-{task.strip('\n')}")
-        case 'edit':
-            with open('Files/todo_list.txt') as file:
-                todo_list = file.readlines()
+        for index, task in enumerate(todo_list):
+            print(f"{index + 1}-{task.strip('\n')}")
 
-            todo_num = int(input("Enter your todo number: ")) - 1
-            todo_list[todo_num] = input("Enter your new todo: ") + '\n'
+    elif 'edit' in user_action:
+        with open('Files/todo_list.txt') as file:
+            todo_list = file.readlines()
 
-            with open('Files/todo_list.txt', 'w') as file:
-                file.writelines(todo_list)
+        todo_num = int(input("Enter your todo number: ")) - 1
+        todo_list[todo_num] = input("Enter your new todo: ") + '\n'
 
-        case 'complete':
-            with open('Files/todo_list.txt') as file:
-                todo_list = file.readlines()
+        with open('Files/todo_list.txt', 'w') as file:
+            file.writelines(todo_list)
 
-            todo_num = int(input("Enter your todo number: ")) - 1
-            todo_list.pop(todo_num)
+    elif 'complete' in user_action:
+        with open('Files/todo_list.txt') as file:
+            todo_list = file.readlines()
 
-            with open('Files/todo_list.txt', 'w') as file:
-                file.writelines(todo_list)
-        case "exit":
-            break
+        todo_num = int(input("Enter your todo number: ")) - 1
+        todo_list.pop(todo_num)
+
+        with open('Files/todo_list.txt', 'w') as file:
+            file.writelines(todo_list)
+
+    elif 'exit' in user_action:
+        break
+
+    else:
+        print("Wrong operation!!!")
+
 
 print("End of operation")
